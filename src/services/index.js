7c49c72751d1dec1a81327ba160ea004623d84a3
Danrley <61889582+danrleylfd@users.noreply.github.com>
2025-04-14 21:20:39 -0300

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const api = axios.create({ baseURL: "https://denkitsu.up.railway.app" });
+const api = axios.create({ baseURL: "https://atalho.up.railway.app" });
 // const api = axios.create({ baseURL: 'https://atalho.glitch.me'});
 
 api.defaults.headers.common["Content-Type"] = "application/json";
