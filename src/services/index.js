493ca068eae0fcb07dd897f4644367ee259f7d0d
Danrley <danrley.reply+github@gmail.com>
2025-04-20 16:45:26 -0300

    update 2025

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8080" });
-//  const api = axios.create({ baseURL: "https://denkitsu.up.railway.app" });
+// const api = axios.create({ baseURL: "http://localhost:8080" });
+ const api = axios.create({ baseURL: "https://denkitsu.up.railway.app" });
 // const api = axios.create({ baseURL: 'https://denkitsu.glitch.me'});
 
 api.defaults.headers.common["Content-Type"] = "application/json";
